025ccdab9299aaa027bc40ed83235d12f8d1bb6f
Michael R <mruhaak@gmail.com>
2020-06-29 03:40:09 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,5 +16,10 @@ require 'pry'
 
 def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
+    contact_details)hash.each do |attribute, data|
   binding.pry
 end
+end
+end
+
